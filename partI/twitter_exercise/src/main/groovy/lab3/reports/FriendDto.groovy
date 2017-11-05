408f8c323e5fee61d76e398c65eb4ace316ae49a
AlHasan AlSarraj <AlHasanAlSarraj@AlHasans-MacBook-Pro.local>
2017-11-05 12:54:42 +0000

    Done

--- partI/twitter_exercise/src/main/groovy/lab3/reports/FriendDto.groovy
+++
@@ -1,9 +0,0 @@
-package lab3.reports
-
-class FriendDto {
-	String name
-	String description
-	int noTweets
-	int noFavoriteTweets
-	int noPopularTweets
-}
--- partII/readme.md
+++
@@ -1,1 +0,0 @@
-# Part II: Cloud Automation using Model-Driven Engineering
