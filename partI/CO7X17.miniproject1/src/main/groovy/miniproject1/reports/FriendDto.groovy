7fdd3da2e7561138dd1bc6a2e6ad2797b12e1c88
AlHasan AlSarraj <AlHasanAlSarraj@alhasans-mbp.cust.hyperoptic.com>
2018-01-14 18:23:25 +0000

    Done

--- partI/CO7X17.miniproject1/src/main/groovy/miniproject1/reports/FriendDto.groovy
+++
@@ -1,9 +0,0 @@
-package miniproject1.reports
-
-class FriendDto {
-	String name
-	String description
-	int noTweets
-	int noRetweets
-	int noActiveTweets
-}
--- partI/CO7X17.miniproject1/twitterFetchTweets.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-twurl "/1.1/statuses/user_timeline.json?user_id=$1&count=$2" | jq . 
